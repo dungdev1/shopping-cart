cd1b6247ac10f6d9a9d64b3497533ff9a666fc9a
Dung Nguyen <dungnguyen9599@gmail.com>
2019-09-01 12:21:25 +0700

    Create product model for database

---
+++ models/product.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+  imagePath: { type: String, required: true},
+  title: { type: String, required: true},
+  description: { type: String, required: true},
+  price: { type: Number, required: true}
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
